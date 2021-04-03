d5401b432ea3d8ce3f4111a89cf25301ee240609
Noah Ryan <nsmryan@gmail.com>
2021-04-03 10:31:21 -0400

    Initial commit

--- build.zig
+++ build.zig
@@ -2,15 +2,18 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("sdl-zig-demo", "src/main.zig");
+    const exe = b.addExecutable("sdl-zig-game", "src/main.zig");
+
     exe.setBuildMode(mode);
+    exe.addIncludeDir(".");
+    exe.addLibPath("lib");
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("c");
 
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 
-    const run = b.step("run", "Run the demo");
+    const run = b.step("run", "Run the game");
     const run_cmd = exe.run();
     run.dependOn(&run_cmd.step);
 }
